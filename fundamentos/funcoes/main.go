5a9d88e14b72f099ce98a9286bdd4404341f6432
Calaça <matheusfcalaca@gmail.com>
2019-10-15 21:32:22 -0300

    finalizando operadores

---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,7 @@
+package main
+
+// não reconhece o import de funçoes olha depois se não e por conta de esta com o gopaht em outro local
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
